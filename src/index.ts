e86850cf0f2072600badb0bf7ef015a616cbf219
erikmiqueias <erikmiqueiaspereira@outlook.com>
2026-03-19 23:40:53 -0400

    chore: add fastify swagger and hello world route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,68 @@
-console.log("Hello, World!");
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
+import Fastify from "fastify";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { z } from "zod";
+
+const app = Fastify({
+  logger: true,
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Conecta + Backend API",
+      description: "Servidor para a aplicação Conecta +",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        description: "Servidor de desenvolvimento",
+        url: `http://localhost:${process.env.PORT || 3000}`,
+      },
+    ],
+  },
+  transform: jsonSchemaTransform,
+
+  // You can also create transform with custom skiplist of endpoints that should not be included in the specification:
+  //
+  // transform: createJsonSchemaTransform({
+  //   skipList: [ '/documentation/static/*' ]
+  // })
+});
+
+await app.register(fastifySwaggerUI, {
+  routePrefix: "/docs",
+});
+
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: "GET",
+  url: "/",
+  schema: {
+    description: "Hello world",
+    tags: ["Hello World"],
+    response: {
+      200: z.object({
+        message: z.string(),
+      }),
+    },
+  },
+  handler: () => {
+    return { message: "Hello world" };
+  },
+});
+
+try {
+  await app.listen({ port: +process.env.PORT! || 3000 });
+} catch (err) {
+  app.log.error(err);
+  process.exit(1);
+}
