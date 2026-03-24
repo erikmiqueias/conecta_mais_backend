18032fbbce867ffd4632f119dd45bbde99af6024
erikmiqueias <erikmiqueiaspereira@outlook.com>
2026-03-24 19:40:30 -0400

    feat: add InputCreateUserDTO and OutputCreateUserDTO

---
+++ src/dtos/user.dto.ts
@@ -0,0 +1,17 @@
+import { Role } from "../generated/prisma/enums.js";
+
+export interface InputCreateUserDTO {
+  username: string;
+  email: string;
+  password: string;
+  role: Role;
+}
+
+export interface OutputCreateUserDTO {
+  id: string;
+  username: string;
+  email: string;
+  role: Role;
+  createdAt: Date;
+  updatedAt: Date;
+}
