5885e73e31205117139d4d98568f701321c5a860
erikmiqueias <erikmiqueiaspereira@outlook.com>
2026-03-24 19:40:05 -0400

    feat: add UserSchema and ErrorSchema

---
+++ src/schemas/index.ts
@@ -0,0 +1,30 @@
+import { z } from "zod";
+export const UserSchema = z.object({
+  id: z.uuid({ error: "Invalid UUId" }),
+  username: z.string().trim().min(1, {
+    error: "Username is required",
+  }),
+  email: z.email({
+    message: "Invalid email",
+  }),
+  password: z
+    .string()
+    .min(1, {
+      error: "Password is required",
+    })
+    .max(8, {
+      error: "Password must be less than 8 characters",
+    }),
+  role: z
+    .enum(["USER", "ORGANIZER"], {
+      error: "Invalid role",
+    })
+    .default("USER"),
+  createdAt: z.date().optional(),
+  updatedAt: z.date().optional(),
+});
+
+export const ErrorSchema = z.object({
+  message: z.string(),
+  code: z.string(),
+});
