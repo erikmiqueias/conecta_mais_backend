d1e0a9773667c90b17ce319ad8d04dbc63341a4f
erikmiqueias <erikmiqueiaspereira@outlook.com>
2026-03-26 14:37:56 -0400

    feat: add get user by id use case

---
+++ src/interfaces/user/usecases/get-user-by-id.ts
@@ -0,0 +1,5 @@
+import { OutputGetUserByIdDTO } from "../../../dtos/user.dto.js";
+
+export interface IGetUserByIdUseCase {
+  execute(userId: string): Promise<OutputGetUserByIdDTO | null>;
+}
---
+++ src/usecases/user/get-user-by-id.ts
@@ -0,0 +1,17 @@
+import { OutputGetUserByIdDTO } from "../../dtos/user.dto.js";
+import { UserNotFoundError } from "../../errors/errors.js";
+import { IGetUserByIdRepository } from "../../interfaces/user/repositories/get-user-by-id.js";
+import { IGetUserByIdUseCase } from "../../interfaces/user/usecases/get-user-by-id.js";
+
+export class GetUserByIdUseCase implements IGetUserByIdUseCase {
+  constructor(private readonly getUserByIdRepository: IGetUserByIdRepository) {}
+  async execute(userId: string): Promise<OutputGetUserByIdDTO | null> {
+    const user = await this.getUserByIdRepository.execute(userId);
+
+    if (!user) {
+      throw new UserNotFoundError();
+    }
+
+    return user;
+  }
+}
