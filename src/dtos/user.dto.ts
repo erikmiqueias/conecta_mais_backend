2ac563e42b69535cefa645c804812b41cfadbc9b
erikmiqueias <erikmiqueiaspereira@outlook.com>
2026-03-28 22:50:29 -0400

    feat: add UpdateUserRepository

--- src/dtos/user.dto.ts
+++ src/dtos/user.dto.ts
@@ -3,6 +3,8 @@ import { z } from "zod";
 import {
   CreateUserInputSchema,
   GetUserByEmailOutputSchema,
+  UpdateUserInputSchema,
+  UpdateUserOutputSchema,
   UserOutputSchema,
 } from "../schemas/user.schema.js";
 
@@ -12,3 +14,11 @@ export type OutputGetUserByIdDTO = z.infer<typeof UserOutputSchema>;
 export type OutputGetUserByEmailDTO = z.infer<
   typeof GetUserByEmailOutputSchema
 >;
+
+export type InputUpdateUserDTO = Omit<
+  z.infer<typeof UpdateUserInputSchema>,
+  "role"
+>;
+export type OutputUpdateUserDTO = z.infer<typeof UpdateUserOutputSchema>;
+
+console.log();
---
+++ src/repositories/user/update-user.ts
@@ -0,0 +1,24 @@
+import {
+  InputUpdateUserDTO,
+  OutputUpdateUserDTO,
+} from "../../dtos/user.dto.js";
+import { IUpdateUserRepository } from "../../interfaces/user/repositories/update-user.js";
+import { prisma } from "../../lib/db.js";
+
+export class UpdateUserRepository implements IUpdateUserRepository {
+  async execute(
+    userId: string,
+    data: InputUpdateUserDTO,
+  ): Promise<OutputUpdateUserDTO | null> {
+    const user = await prisma.user.update({
+      data: {
+        email: data.email,
+        username: data.username,
+      },
+      where: {
+        id: userId,
+      },
+    });
+    return user;
+  }
+}
