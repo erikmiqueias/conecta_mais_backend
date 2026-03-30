880ab4282dafef3e66fa3aadce1dbb262cd8470d
erikmiqueias <erikmiqueiaspereira@outlook.com>
2026-03-30 16:55:21 -0400

    feat: add errors handlers to OpenStreetMapProvider

--- src/shared/errors/errors.ts
+++ src/shared/errors/errors.ts
@@ -9,3 +9,21 @@ export class UserNotFoundError extends Error {
     super("User not found");
   }
 }
+
+export class AddressNotFoundError extends Error {
+  constructor() {
+    super("Address not found for the given coordinates");
+  }
+}
+
+export class CoordinatesNotFoundError extends Error {
+  constructor() {
+    super("Coordinates not found for the given address");
+  }
+}
+
+export class OSMProviderError extends Error {
+  constructor(message: string) {
+    super(message);
+  }
+}
