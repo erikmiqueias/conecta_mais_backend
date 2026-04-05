68dd19a63acd6290d971ae883ed39dbecc24ef7e
erikmiqueias <erikmiqueiaspereira@outlook.com>
2026-04-05 02:42:38 -0400

    feat: add status param in get event by id repository

--- src/modules/event/repositories/get-event-by-id.repo.ts
+++ src/modules/event/repositories/get-event-by-id.repo.ts
@@ -1,17 +1,22 @@
+import { EventStatus } from "@generated/prisma/enums.js";
 import { prisma } from "@shared/lib/db.js";
 
 import { IGetEventByIdRepository } from "./interfaces/index.js";
 
 export class GetEventByIdRepository implements IGetEventByIdRepository {
-  async execute(
-    eventId: string,
-  ): Promise<{ organizerId: string; id: string; endDateTime: Date } | null> {
+  async execute(eventId: string): Promise<{
+    organizerId: string;
+    id: string;
+    endDateTime: Date;
+    status: EventStatus;
+  } | null> {
     const event = await prisma.event.findUnique({
       where: { id: eventId },
       select: {
         id: true,
         organizerId: true,
         endDateTime: true,
+        status: true,
       },
     });
     return event;
--- src/modules/event/repositories/interfaces/get-event-by-id.interface.ts
+++ src/modules/event/repositories/interfaces/get-event-by-id.interface.ts
@@ -1,5 +1,10 @@
+import { EventStatus } from "@generated/prisma/enums.js";
+
 export interface IGetEventByIdRepository {
-  execute(
-    eventId: string,
-  ): Promise<{ organizerId: string; id: string; endDateTime: Date } | null>;
+  execute(eventId: string): Promise<{
+    organizerId: string;
+    id: string;
+    endDateTime: Date;
+    status: EventStatus;
+  } | null>;
 }
