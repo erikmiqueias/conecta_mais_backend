386166f7819fdcfc144f3aa5429f9d538c0cf559
erikmiqueias <erikmiqueiaspereira@outlook.com>
2026-04-13 01:06:07 -0400

    refactor: add optional variables in verify-optional-jwt type

--- src/shared/middlewares/verify-optional-jwt.ts
+++ src/shared/middlewares/verify-optional-jwt.ts
@@ -2,13 +2,11 @@ import { FastifyReply, FastifyRequest } from "fastify";
 
 export const verifyOptionalJwt = async (
   request: FastifyRequest,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  reply: FastifyReply,
+  _reply: FastifyReply,
 ) => {
   try {
     await request.jwtVerify();
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (error) {
-    // If the token is invalid or not provided, we simply ignore the error
+  } catch (_error) {
+    // do nothing
   }
 };
